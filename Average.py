0067a5731435c73fc97ff365be8bafe91e644398
divyesh-mali <125090897+divyesh-mali@users.noreply.github.com>
2023-09-04 11:29:25 +0530

    Update Average.py

--- Average.py
+++ Average.py
@@ -1,10 +1,28 @@
-marklist = [25, 30, 'a', 21, 23, 30]
-def average():
+def average(n):
     sum=0
-    average=0
-    for i in range(len(marklist)):
-        if marklist[i] != 'a':
-            sum = sum + marklist[i]
-            print(sum)
-            average = sum // n
-            print(average)
+    avg=0
+    for i in range(n):
+        if marklist[i] != 'A':
+            sum = sum + int(marklist[i])
+        print(sum)
+        average = sum // n
+        print("The average of marks is: ", average)
+
+marklist=[]
+n = int(input("Enter number of students: "))
+
+print("Enter marks of students. Enter 'A' if student is absent.")
+
+for i in range(n):
+    marks=input("Enter marks: ")
+    marklist.append(marks)
+print(marklist)
+
+while True:
+    print("1. Average\n 2. Count of absent students\n 3. Lowest & highest marks\n"
+          "4. Marks with highest frequency\n")
+
+    ch = int(input("Enter the choice: "))
+
+    if ch==1:
+        average(n)
